a8d8943f6e64b89988762b56efc9b343a8991513
sh0hzy <149383047+sh0hzy@users.noreply.github.com>
2024-08-29 00:31:40 +0400

    Update app.py

--- sklad01/app.py
+++ sklad01/app.py
@@ -101,4 +101,4 @@ def download_file(filename):
 if __name__ == "__main__":
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+app.run(debug=True)
